86b4a87ccfd1574f8de0f4f78fe3b4e6f340ee22
kevin.erd <kevin.erd@kakaopaycorp.com>
2023-06-28 22:22:25 +0900

    feat: 기본적인 키오스크 기능 구현
    - 기본적으로 사용되는 키오스크의 기능 구현

--- src/main/java/sample/cafekiosk/unit/CafeKiosk.java
+++ src/main/java/sample/cafekiosk/unit/CafeKiosk.java
@@ -1,16 +1,40 @@
 package sample.cafekiosk.unit;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * 카페 키오스크
  */
 public class CafeKiosk {
     /**
+     * 주문한 음료 목록
+     */
+    private final List<Beverage> beverages = new ArrayList<>();
+
+    /**
      * 주문 (음료 추가)
      *
-     * @param beverage 음료 종류
+     * @param beverage 음료
      */
     public void add(final Beverage beverage) {
+        this.beverages.add(beverage);
+    }
 
+    /**
+     * 주문 취소 (음료 제거)
+     *
+     * @param beverage 음료
+     */
+    public void remove(final Beverage beverage) {
+        this.beverages.remove(beverage);
+    }
+
+    /**
+     * 주문 전체 취소 (음료 전체 제거)
+     */
+    public void clear() {
+        this.beverages.clear();
     }
 
     /**
@@ -19,6 +43,8 @@ public class CafeKiosk {
      * @return 카페 키오스크를 통해 주문한 음료들의 가격 총합
      */
     public int calculateTotalPrice() {
-        return 0;
+        return this.beverages.stream()
+                .mapToInt(Beverage::getPrice)
+                .sum();
     }
 }
